6ae76076553f25d3506f6ab231e4dc08262b18c2
Junyoung Shin <jun021222@naver.com>
2024-07-15 09:50:11 +0900

    Update samsum_dataset.py

--- src/llama_recipes/datasets/samsum_dataset.py
+++ src/llama_recipes/datasets/samsum_dataset.py
@@ -26,10 +26,17 @@ def get_preprocessed_samsum(dataset_config, tokenizer, split):
         prompt = tokenizer.encode(tokenizer.bos_token + sample["prompt"], add_special_tokens=False)
         summary = tokenizer.encode(sample["summary"] +  tokenizer.eos_token, add_special_tokens=False)
 
-        sample = {
-            "input_ids": prompt + summary,
-            "attention_mask" : [1] * (len(prompt) + len(summary)),
-            "labels": [-100] * len(prompt) + summary,
+        if split == 'train':
+            sample = {
+                "input_ids": prompt + summary,
+                "attention_mask" : [1] * (len(prompt) + len(summary)),
+                "labels": [-100] * len(prompt) + summary,
+            }
+        else:
+            sample = {
+                "input_ids": prompt,
+                "attention_mask": [1] * len(prompt),
+                "labels": summary
             }
 
         return sample
